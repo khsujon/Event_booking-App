2945aee392d9d4bf085d20c871be653629e2f936
khsujon <kamrulhasan.km3@gmail.com>
2025-01-10 18:54:27 +0600

    Profile page added

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -10,6 +10,12 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile Page"),
+        centerTitle: true,
+      ),
+    );
+    ;
   }
 }
