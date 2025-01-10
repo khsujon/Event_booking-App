629942297a1394ec951e882e393bfe297504547c
khsujon <kamrulhasan.km3@gmail.com>
2025-01-10 18:53:47 +0600

    Booking page added

--- lib/pages/booking_screen.dart
+++ lib/pages/booking_screen.dart
@@ -10,6 +10,11 @@ class BookingScreen extends StatefulWidget {
 class _BookingScreenState extends State<BookingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Booking Page"),
+        centerTitle: true,
+      ),
+    );
   }
 }
