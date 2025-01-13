dc4ce8615f8067dd4dbe741d4880e25074672a76
khsujon <kamrulhasan.km3@gmail.com>
2025-01-13 14:06:12 +0600

    Google auth added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:book_event/pages/bottom_navbar.dart';
 import 'package:book_event/pages/details_page.dart';
 import 'package:book_event/pages/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
