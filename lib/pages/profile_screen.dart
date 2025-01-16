d76c991ead6f029129e664727a8ddc2be2dbd16b
khsujon <kamrulhasan.km3@gmail.com>
2025-01-16 23:07:54 +0600

    profile added

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:book_event/pages/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -15,6 +16,72 @@ class _ProfileScreenState extends State<ProfileScreen> {
         title: Text("Profile Page"),
         centerTitle: true,
       ),
+      body: Container(
+          child: Column(
+        children: [
+          Image.asset("images/onboarding.png"),
+          SizedBox(),
+          Text(
+            "Book Your Favorite Event",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "Near You!!",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:
+                Text("Discover, book and explore the best events in your city",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    )),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          //Google Signup Button
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => BottomNavbar(),
+                ),
+              );
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.deepPurple,
+              ),
+              child: Center(
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
     );
     ;
   }
